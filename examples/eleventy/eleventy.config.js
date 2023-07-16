8da7aac56715fdefaff0891e9d6ef54916dfa89f
Wesley Luyten <me@wesleyluyten.com>
2023-07-16 08:28:38 -0500

    example: add Eleventy

---
+++ examples/eleventy/eleventy.config.js
@@ -0,0 +1,12 @@
+module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addTransform("wesc", async function(content) {
+
+    const { renderToString } = await import('wesc/server');
+
+    await import('media-chrome');
+    await import('media-chrome/dist/media-theme-element.js');
+
+    return renderToString(content);
+  });
+};
